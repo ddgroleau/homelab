039b2921f48bdf4a95143c9a73a5d47d55f63cd2
Dan Groleau <ddgroleau@gmail.com>
2026-03-03 11:17:05 -0500

    Add calico installation patch

--- bootstrap-cluster/bash/update.sh
+++ bootstrap-cluster/bash/update.sh
@@ -5,5 +5,5 @@ echo -e "Updating all homelab nodes...\n"
 
 for node in controlplane worker01 worker02 worker03 worker04 worker05; do
     echo "Updating $node..."
-    ssh -o BatchMode=yes -o ConnectTimeout=5 "$node@$node" "sudo apt update && sudo apt upgrade"
+    ssh -o BatchMode=yes -o ConnectTimeout=5 "$node@$node" "sudo apt update && sudo apt upgrade -y"
 done
